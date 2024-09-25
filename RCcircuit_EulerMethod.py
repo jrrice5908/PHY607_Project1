80a35c7aae745ac4f6c54190683b3c993a2e56ae
jrricesyr <jrrice@syr.edu>
2024-09-25 13:57:22 -0400

    working code for eulers method

---
+++ RCcircuit_EulerMethod.py
@@ -0,0 +1,29 @@
+## ODE
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def update(charge,step,T):
+    Qnew = np.add(charge,-(charge/T)*step)
+    return Qnew
+        
+R = 10
+C = 15     
+T = R + C  
+
+charge = np.array([1])     #initial charge Q(0)=0
+step = 0.001
+Q = []
+
+for i in range(100000):                              #EXPLICIT
+    charge = update(charge,step,T)
+    Q.append(charge)
+t = np.arange(len(Q))*step
+
+plt.plot(t,Q)
+plt.ylabel('Q, coulombs')
+plt.xlabel('time, seconds')
+plt.title("Charge on Capacitor vs Time")
+plt.show
+
+plt.savefig("Charg_vs_Time_Cap.png")
