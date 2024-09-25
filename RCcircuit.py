7818ac3586c22ec1cd05f5b68147a40b2ccc4af2
jrricesyr <jrrice@syr.edu>
2024-09-25 12:28:04 -0400

    code edits

--- RCcircuit.py
+++ RCcircuit.py
@@ -4,8 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def update(I,charge,step,T):
-    Qnew = np.add(Q, I*step)
-    Inew = np.add(I, -Qnew/T)
+    Qnew = np.add(charge, I*step)
+    #Inew = np.add(I, -Qnew/T)
+    return Qnew
     
 
 V = 100     #some initial potential
@@ -13,20 +14,17 @@ R = 10      #some resistance
 C = 15      #some capacitance
 T = R + C   #time constant
 
-I = np.array(0.05)
-charge = np.array(0)     #initial charge Q(0)=0
+I = np.array([0.05])
+charge = np.array([0])     #initial charge Q(0)=0
 step = 0.001
 Q = []
 
 for i in range(100000):                              #EXPLICIT
-    I, charge = update(I,charge,step,T)
+    charge = update(I,charge,step,T)
     Q.append(charge)
-t = np.arange(len(x))*step
+t = np.arange(len(Q))*step
 
-plt.plot(t,x)
+plt.plot(t,Q)
 plt.ylabel('Q, coulombs')
 plt.xlabel('time, seconds')
 plt.show
-
-
-# Please enter the commit message for your changes. Lines starting
