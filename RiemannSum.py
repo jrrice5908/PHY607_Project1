c7d8ac42c8fc508e1088093dc4ec48336924a79c
jrricesyr <jrrice@syr.edu>
2024-09-26 19:46:38 -0400

    working on code

--- RiemannSum.py
+++ RiemannSum.py
@@ -1,1 +1,41 @@
-Riemann Sum
+## Integral Reimann Sum
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def ChargeDist(x,y,z):
+    charge = (2C/(a**4))((x*z)/((y+a)**2)
+    return charge
+
+def ReimannSum
+                   
+                   
+step_vol = [1,1,1]
+
+xmin = 0
+xmax = 20
+x=[]                        
+
+ymin = 0
+ymax = 30
+y=[]                         
+                         
+zmin = 0
+zmax = 50
+z=[]                         
+                         
+a = 10
+C = 1
+totCharge = []
+                   
+xint = ((xmax-xmin)/step_vol[0]
+yint = ((ymax-ymin)/step_vol[1]        
+zint = ((zmax-zmin)/step_vol[2]
+
+                        
+def populate(alist,amin,amax,aint):                                    
+    for i in range(amin,amax+1,aint):
+        a_list.append(i+aint)
+    print (alist) 
