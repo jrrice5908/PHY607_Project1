2b88bb695f76e1cb33c7827b2708fb81839a0beb
jrricesyr <jrrice@syr.edu>
2024-09-27 11:08:56 -0400

    working code

--- RiemannSum.py
+++ RiemannSum.py
@@ -3,39 +3,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 def ChargeDist(x,y,z):
-    charge = (2C/(a**4))((x*z)/((y+a)**2)
+    charge = (4*C/(a**4))*((x*z)/(y+a)**2)
     return charge
-
-def ReimannSum
+   
                    
-                   
-step_vol = [1,1,1]
+step_vol = 1
 
 xmin = 0
-xmax = 20
-x=[]                        
+xmax = 20                       
 
 ymin = 0
-ymax = 30
-y=[]                         
+ymax = 30                       
                          
 zmin = 0
-zmax = 50
-z=[]                         
+zmax = 50                       
                          
 a = 10
 C = 1
-totCharge = []
-                   
-xint = ((xmax-xmin)/step_vol[0]
-yint = ((ymax-ymin)/step_vol[1]        
-zint = ((zmax-zmin)/step_vol[2]
 
-                        
-def populate(alist,amin,amax,aint):                                    
-    for i in range(amin,amax+1,aint):
-        a_list.append(i+aint)
-    print (alist) 
+totCharge = 0
+
+for i in range(xmin,xmax):
+        for j in range(ymin,ymax):
+            for k in range(zmin,zmax):
+                pointCharge = ChargeDist(i,j,k)*step_vol
+                totCharge = totCharge + pointCharge
+        
+print (totCharge)
