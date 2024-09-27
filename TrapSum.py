793a828b7445035455dfd40c55cc026282dc02f5
jrricesyr <jrrice@syr.edu>
2024-09-27 11:20:34 -0400

    working Trapezoidal code

--- TrapSum.py
+++ TrapSum.py
@@ -1,1 +1,31 @@
-TrapezoidalSum
+## Trapezoidal Rule
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ChargeDist(x,y,z):
+    charge = (4*C/(a**4))*((x*z)/(y+a)**2)
+    return charge
+
+xmin = 0
+xmax = 20                       
+
+ymin = 0
+ymax = 30                       
+                         
+zmin = 0
+zmax = 50                       
+                         
+a = 10
+C = 1
+
+totCharge = 0    
+    
+for i in range(xmin,xmax,step_vol):
+        for j in range(ymin,ymax,step_vol):
+            for k in range(zmin,zmax,step_vol):
+                pointCharge = ((ChargeDist(i-1,j-1,k-1)+ChargeDist(i,j,k))/2)*step_vol
+                totCharge = totCharge + pointCharge    
+    
+    
+print ("Total Charge = ",totCharge)
