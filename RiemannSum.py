8b1a2de38a803f3659a49a7f788552dd637eb8d6
jrricesyr <jrrice@syr.edu>
2024-09-27 15:20:31 -0400

    formatting

--- RiemannSum.py
+++ RiemannSum.py
@@ -1,14 +1,13 @@
-## Integral Reimann Sum
+# Integral Reimann Sum
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-def ChargeDist(x,y,z):
+def ChargeDist(x,y,z):		#This is a three dimensional volume charge distribution in certesian coordinates			
     charge = (4*C/(a**4))*((x*z)/(y+a)**2)
     return charge
-   
-                   
-step_vol = 1
+                      
+step_vol = 1			#This is analogous to step size in spherical cow problem, all values are in cm 
 
 xmin = 0
 xmax = 20                       
@@ -18,16 +17,17 @@ ymax = 30
                          
 zmin = 0
 zmax = 50                       
-                         
+
+tot_vol=(xmax-xmin)*(ymax-ymin)*(zmax-zmin)                         
 a = 10
 C = 1
 
-totCharge = 0
+totCharge = 0			#Giving charge an initial value such that I can call it in following algorithm
 
-for i in range(xmin,xmax,step_vol):
-        for j in range(ymin,ymax,step_vol):
-            for k in range(zmin,zmax,step_vol):
-                pointCharge = ChargeDist(i,j,k)*step_vol
+for i in range(xmin, xmax, step_vol):
+        for j in range(ymin, ymax, step_vol):
+            for k in range(zmin, zmax, step_vol):
+                pointCharge = ChargeDist(i, j, k)*step_vol
                 totCharge = totCharge + pointCharge
         
-print ("Total Charge = ",totCharge)
+print ("Total Charge = ", totCharge, "across total volume,", tot_vol, "cm")
