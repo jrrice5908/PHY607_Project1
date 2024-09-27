69649755bcae4cf96b66a117a31849bb23c60e78
jrricesyr <jrrice@syr.edu>
2024-09-27 16:20:01 -0400

    ICs

--- TrapSum.py
+++ TrapSum.py
@@ -32,3 +32,30 @@ for i in range(xmin,xmax,step_vol):
     
     
 print ("Total Charge = ", totCharge, "C", "across total volume,", tot_vol,"cm^3")
+
+#Different Initial Conditions
+
+xmin = 0
+xmax = 64
+
+ymin = 0
+ymax = 72
+
+zmin = 0
+zmax = 30
+
+tot_vol=(xmax-xmin)*(ymax-ymin)*(zmax-zmin)
+a = 10
+C = 1
+
+totCharge = 0   #In units of Coulombs 
+
+for i in range(xmin,xmax,step_vol):
+        for j in range(ymin,ymax,step_vol):
+            for k in range(zmin,zmax,step_vol):
+                pointCharge = ((ChargeDist(i-1,j-1,k-1)+ChargeDist(i,j,k))/2)*step_vol
+                totCharge = totCharge + pointCharge
+
+
+print ("Total Charge = ", totCharge, "C", "across total volume,", tot_vol,"cm^3")
+
